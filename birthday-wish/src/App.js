e57d498d6a5282f2abea89472fbf5bc3f2d9b95a
Hasini Sankalpana <156077759+Hasini-Sankalpana@users.noreply.github.com>
2024-08-23 13:45:58 +0530

    Birthday

---
+++ birthday-wish/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import BirthdayWish from './components/BirthdayWish';
+
+function App() {
+  return (
+    <div className="App">
+      <BirthdayWish />
+    </div>
+  );
+}
+
+export default App;
